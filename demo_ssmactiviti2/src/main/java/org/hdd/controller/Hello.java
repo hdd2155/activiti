60e4ee12263f1004babc137a7e04a5afc234365d
hdd <949217272@qq.com>
2018-02-22 15:22:30 +0800

    1

---
+++ demo_ssmactiviti2/src/main/java/org/hdd/controller/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * Create Date:2018年2月22日
+ */
+package org.hdd.controller;
+
+/**
+ * <br>Title:TODO 类标题
+ * <br>Description:TODO 类功能描述
+ * <br>Author:何端端(949217272@qq.com)
+ * <br>Date:2018年2月22日
+ */
+public class Hello {
+
+}
